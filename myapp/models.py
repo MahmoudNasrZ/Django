4aed4363d16a8c166d49f70509241272de91c79f
MahmoudNasrZ <mahmoudn.zalabany256@gmail.com>
2025-02-28 01:24:53 +0200

    Day1-task Full+Extra

--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,13 @@ class ClassRoom(models.Model):
     name = models.CharField(verbose_name="Class Name", max_length=50, unique=True)
     subject = models.CharField(verbose_name="Subject", max_length=50)
     year = models.IntegerField(verbose_name="Year")
+
+    def __str__(self):
+        return f"{self.name} - {self.subject} ({self.year})"
+
+class ClassArea(models.Model):
+    length = models.IntegerField(verbose_name="Class Length")
+    width = models.IntegerField(verbose_name="Class Width")
+
+    def __str__(self):
+        return f"Class Area: {self.length} x {self.width}"
